ffe77028684588b23936131a975015732d58c703
reselbob <reselbob@gmail.com>
2021-02-05 18:50:10 -0800

    got wisesaying happening

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::io::prelude::*;
-use std::io::BufReader;
+//use std::io::prelude::*;
 use std::fs::File;
-use std::path::Path;
+use std::io::BufRead;
+use std::io::BufReader;
+//use std::path::Path;
+use rand::seq::IteratorRandom;
 
 fn main() -> std::io::Result<()> {
-    let fname = "src/fortunes.txt";
-    let path = Path::new(fname);
-    let f = File::open(path)?;
-    let mut reader = BufReader::new(f);
-
-    let mut line = String::new();
-    let len = reader.read_line(&mut line)?;
-    println!("First line is {} bytes long", len);
+    let mut str = getRandomSaying();
+    println!("{}", str);
     Ok(())
+}
+
+fn getRandomSaying() -> String {
+    let fname = "src/fortunes.txt";
+    let f = File::open(fname).unwrap_or_else(|e| panic!("(;_;) file not found: {}: {}", fname, e));
+    let f = BufReader::new(f);
+
+    let lines = f.lines().map(|l| l.expect("Couldn't read line"));
+
+    let mut line = lines
+        .choose(&mut rand::thread_rng())
+        .expect("File had no lines");
+    return line;
 }
