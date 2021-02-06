4924a43b1e70f0e028b02a18f4c7f6c3b8abb80a
reselbob <reselbob@gmail.com>
2021-02-05 18:50:40 -0800

    got wisesaying happening

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::io::BufReader;
 use rand::seq::IteratorRandom;
 
 fn main() -> std::io::Result<()> {
-    let mut str = getRandomSaying();
+    let mut str = getWiseSaying();
     println!("{}", str);
     Ok(())
 }
 
-fn getRandomSaying() -> String {
+fn getWiseSaying() -> String {
     let fname = "src/fortunes.txt";
     let f = File::open(fname).unwrap_or_else(|e| panic!("(;_;) file not found: {}: {}", fname, e));
     let f = BufReader::new(f);
