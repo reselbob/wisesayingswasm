37a66fab45858f5cb1060b64225e738fee416bf0
Bob Reselman <bob@CogArtTech.com>
2021-02-09 14:22:57 -0800

    Made the TS files WASM happy

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
-const wasmCode = await Deno.readFile("./target/wasm32-unknown-unknown/debug/wisesayings.wasm");
-const wasmModule = new WebAssembly.Module(wasmCode);
-const wasmInstance = new WebAssembly.Instance(wasmModule);
+// @deno-types="./wisesayings.d.ts"
+import { get_wise_saying } from "./wisesayings.js";
 
-const saying = wasmInstance.exports.get_wise_saying as CallableFunction
-
-console.log(saying());
-console.log(saying());
-console.log(saying());
-console.log(saying());
+console.log(`${get_wise_saying()}\n`);
+console.log(`${get_wise_saying()}\n`);
+console.log(`${get_wise_saying()}\n`);
