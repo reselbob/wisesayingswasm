10c58c77320cb52ac2ee4fdcf51cd096ef38cf8e
Bob Reselman <bob@CogArtTech.com>
2021-02-09 14:24:53 -0800

    updated file path

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-// @deno-types="./wisesayings.d.ts"
-import { get_wise_saying } from "./wisesayings.js";
+// @deno-types="./deno/wisesayings.d.ts"
+import { get_wise_saying } from "./deno/wisesayings.js";
 
 console.log(`${get_wise_saying()}\n`);
 console.log(`${get_wise_saying()}\n`);
