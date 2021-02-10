b35274e43a220d0c83f3d55cd6ec63808c049141
reselbob <reselbob@gmail.com>
2021-02-10 11:28:19 -0800

    updated cheatsheet

--- main.ts
+++
@@ -1,6 +0,0 @@
-// @deno-types="./deno/wisesayings.d.ts"
-import { get_wise_saying } from "./deno/wisesayings.js";
-
-console.log(`${get_wise_saying()}\n`);
-console.log(`${get_wise_saying()}\n`);
-console.log(`${get_wise_saying()}\n`);
