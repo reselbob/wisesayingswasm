47064a07fde0d384d52a2cfbf0e7ebda18eecec5
reselbob <reselbob@gmail.com>
2021-02-10 12:05:06 -0800

    modified main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,3 @@
-// @deno-types="./wisesayings.d.ts"
 import { serve } from "https://deno.land/std@0.86.0/http/server.ts";
 import { get_wise_saying } from "./wisesayings.js";
 
@@ -13,8 +12,6 @@ const server = serve({ hostname: "0.0.0.0", port});
 console.log(`HTTP webserver running at ${new Date()}.  Access it at:  http://localhost:${port}/`);
 
 for await (const request of server) {
-    let bodyContent = "Your user-agent is:\n\n";
-    bodyContent += request.headers.get("user-agent") || "Unknown";
     const saying = get_wise_saying();
     request.respond({ status: 200, body: saying });
   }
