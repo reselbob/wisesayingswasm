3b823b27a1caedecd2f3a378fedd33a7c9a106be
reselbob <reselbob@gmail.com>
2021-03-06 11:16:07 -0800

    removed gen files in server

--- server/wisesayings.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @returns {string}
-*/
-export function get_wise_saying(): string;
--- server/wisesayings_bg.wasm.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function get_wise_saying(a: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_exn_store(a: number): void;
