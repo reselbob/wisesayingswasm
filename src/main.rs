977a0c13f1ddcc4cf931e04a5856c9f263297434
Zoo Sky <zoosky@gmail.com>
2021-05-01 10:08:14 +0200

    cosmetics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use wisesayings::get_wise_saying;
 
 fn main() -> std::io::Result<()> {
-    let mut str = get_wise_saying();
+    let str = get_wise_saying();
     println!("{}", str);
     Ok(())
 }
